f607fca7a69ed25060c952eeb3b0ded15ceae65b
kangju2000 <kangju2000@gmail.com>
2024-11-02 13:13:26 +0000

    feat: private route redirect

--- apps/client/middleware.ts
+++ apps/client/middleware.ts
@@ -1,23 +1,35 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 import { AUTH_COOKIE_NAME, REFRESH_TOKEN_COOKIE_NAME } from '@/lib/auth/constants';
+import { authApi } from './apis/config/instance';
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get(AUTH_COOKIE_NAME);
-  const isPrivateRoute = [''].includes(request.nextUrl.pathname);
+const PRIVATE_ROUTES = ['/settings', '/mypage'];
 
-  if (isPrivateRoute && token == null) {
-    // refresh token 유효성 검사
-    const refreshToken = request.cookies.get(REFRESH_TOKEN_COOKIE_NAME);
+export async function middleware(request: NextRequest) {
+  // const token = request.cookies.get(AUTH_COOKIE_NAME);
+  const isPrivateRoute = PRIVATE_ROUTES.includes(request.nextUrl.pathname);
 
-    // TODO: refresh token 유효성 검사
-    if (refreshToken) {
-      // 유효하면 access token 재발급
-      // 유효하지 않으면 로그인 페이지로 리다이렉트
+  if (isPrivateRoute) {
+    // access token 유효성 검사
+    try {
+      await authApi.인증인가Api.getProfile();
+
       return NextResponse.next();
+    } catch (error) {
+      return NextResponse.redirect(new URL('/login', request.url));
     }
 
-    return NextResponse.redirect(new URL('/login', request.url));
+    // // refresh token 유효성 검사
+    // const refreshToken = request.cookies.get(REFRESH_TOKEN_COOKIE_NAME);
+
+    // // TODO: refresh token 유효성 검사
+    // if (refreshToken) {
+    //   // 유효하면 access token 재발급
+    //   // 유효하지 않으면 로그인 페이지로 리다이렉트
+    //   return NextResponse.next();
+    // }
+
+    // return NextResponse.redirect(new URL('/login', request.url));
   }
 
   return NextResponse.next();
