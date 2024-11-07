a54bedc893921121b18b10b3f179af648ec0cf3a
kangju2000 <kangju2000@gmail.com>
2024-11-07 03:21:57 +0000

    fix: header fixed

--- apps/client/components/layout/header/header.tsx
+++ apps/client/components/layout/header/header.tsx
@@ -6,12 +6,13 @@ import { authQueries } from '@/apis/auth';
 export function Header() {
   return (
     <PrefetchHydration queries={[authQueries.profile()]}>
-      <header className="bg-background sticky top-0 z-50 border-b">
-        <div className="mx-auto flex min-h-[68px] max-w-[980px] items-center justify-between px-4 py-4">
+      <header className="bg-background fixed top-0 z-50 w-full border-b">
+        <div className="mx-auto flex h-[68px] max-w-[980px] items-center justify-between px-4 py-4">
           <NavigationSection />
           <AuthSection />
         </div>
       </header>
+      <div className="h-[68px]"></div>
     </PrefetchHydration>
   );
 }
